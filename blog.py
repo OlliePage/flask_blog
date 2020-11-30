ad09668d074596de6ad01ce320113f6e289a090c
olliepage <products15816@gmail.com>
2020-11-30 07:59:36 +0000

    add for loop to template

--- blog.py
+++ blog.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Ollie Page',
+        'title': 'Blog post 1',
+        'content': 'first post content',
+        'date_posted': '2020-11-30'
+    },
+    {
+        'author': 'Jane Page',
+        'title': 'Blog post 2',
+        'content': 'second post content',
+        'date_posted': '2020-12-01'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
