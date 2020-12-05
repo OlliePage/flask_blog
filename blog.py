3cb060de0b8116b3f1c359d86e952ad15799217c
olliepage <products15816@gmail.com>
2020-12-05 14:29:36 +0000

    working login and registration

--- blog.py
+++ blog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ce50e9c1e7626766e4b1851d6e4bd3e2'
 
 posts = [
     {
@@ -17,6 +19,7 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -25,7 +28,29 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check your details', 'danger')
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
