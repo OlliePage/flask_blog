e69e768ed2690587b10f98f76ce976eda8fa2dfd
olliepage <products15816@gmail.com>
2020-12-06 12:42:31 +0000

    update profile pic

--- blog/routes.py
+++ blog/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template, url_for, flash, redirect, request
-from blog.forms import RegistrationForm, LoginForm
+from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from blog import app, db, bcrypt
+from PIL import Image
 from blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
+import secrets
+import os
 
 posts = [
     {
@@ -70,7 +72,39 @@ def logout():
     return redirect((url_for('home')))
 
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, file_extension = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + file_extension
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_filename)
+
+    # resize image before saving
+    output_size = (125,125)
+    image = Image.open(form_picture)
+    image.thumbnail(output_size)
+    image.save(picture_path)
+
+    return picture_filename
+
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html',
+                           title='Account',
+                           image_file=image_file,
+                           form=form)
